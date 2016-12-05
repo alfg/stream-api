b19275d0904b1b530797d1228a097c83671b4dd7
alf <alf.g.jr@gmail.com>
2016-12-04 16:59:14 -0800

    database default config.

--- configuration/config.go
+++ configuration/config.go
@@ -8,13 +8,23 @@ import (
 
 // Configuration configuration setup
 type Configuration struct {
-	Host                string `json:"host"`
-	Port                string `json:"port"`
-	JWTKey              string `json:"jwtKey"`
-	StreamServerLiveURL string `json:"streamServerLiveUrl"`
-	StreamThumbnailURL  string `json:"streamThumbnailUrl"`
-	StreamServerRTMPURL string `json:"streamServerRtmpUrl"`
-	StreamVideoURL      string `json:"streamVideoUrl"`
+	Host                string                `json:"host"`
+	Port                string                `json:"port"`
+	JWTKey              string                `json:"jwtKey"`
+	Database            DatabaseConfiguration `json:"db"`
+	StreamServerLiveURL string                `json:"streamServerLiveUrl"`
+	StreamThumbnailURL  string                `json:"streamThumbnailUrl"`
+	StreamServerRTMPURL string                `json:"streamServerRtmpUrl"`
+	StreamVideoURL      string                `json:"streamVideoUrl"`
+}
+
+// DatabaseConfiguration Database configuration setup.
+type DatabaseConfiguration struct {
+	Server   string `json:"server"`
+	Port     string `json:"port"`
+	User     string `json:"user"`
+	Password string `json:"password"`
+	Database string `json:"database"`
 }
 
 // ConfigurationSetup configuration setup
--- data/db.go
+++ data/db.go
@@ -4,15 +4,23 @@ import (
 	_ "database/sql" // Database.
 	"fmt"
 	"log"
+	"os"
+	"stream-api/configuration"
 
 	"github.com/jmoiron/sqlx"
 	_ "github.com/mattn/go-sqlite3" // sqlite3 driver
 )
 
-const (
-	dialect  = "sqlite3"
-	database = "test.db"
-)
+const dialect = "sqlite3"
+
+var database = os.Getenv("DATABASE")
+
+func init() {
+	config := configuration.ConfigurationSetup()
+	if database == "" {
+		database = config.Database.Database
+	}
+}
 
 // ConnectDB Connects to sqlite3 database
 func ConnectDB() (*sqlx.DB, error) {
